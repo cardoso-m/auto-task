fb858978a878bf11fa450b867cb64dc0f7448e71
Matheus <mdonizetecardoso@gmail.com>
2024-10-21 12:21:25 -0300

    Incluindo a exclusão de usuários no banco

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -3,5 +3,7 @@ const router = express.Router()
 const userController = require('../controllers/userController')
 
 router.post('/user', userController.createUser)
+router.get('/user', userController.getUsers)
+router.delete('/user/:id',userController.deleteUser)
 
 module.exports = router
