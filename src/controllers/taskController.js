6afaa845d15636fa1704af545d36eaf41efb9e23
Matheus <mdonizetecardoso@gmail.com>
2024-10-21 17:52:48 -0300

    Criação da rota de edição (PUT) e finalização do CRUD

--- src/controllers/taskController.js
+++ src/controllers/taskController.js
@@ -41,3 +41,23 @@ exports.deleteTask = async (req, res) => {
         res.status(400).send(err)
     }
 }
+
+exports.updateTask = async (req, res) => {
+    var id = req.params.id
+    var {name, description, client_id, priority, expires_in} = req.body
+
+    try {
+        await db('task')
+                .where({"id": id})
+                    .update({
+                        name: name,
+                        description: description,
+                        client_id: client_id,
+                        priority: priority,
+                        expires_in: expires_in
+                    })
+        res.status(200).send('Tarefa editada')
+    } catch (err) {
+        res.status(400).send(err)
+    }
+}
--- src/routes/taskRouter.js
+++ src/routes/taskRouter.js
@@ -2,8 +2,9 @@ const express = require('express')
 const router = express.Router()
 const taskController = require('../controllers/taskController')
 
-router.post('/task',taskController.createTask)
+router.post('/task', taskController.createTask)
 router.get('/task', taskController.getTasks)
 router.delete('/task/:id', taskController.deleteTask)
+router.put('/task/:id', taskController.updateTask)
 
 module.exports = router
