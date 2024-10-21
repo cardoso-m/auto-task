90659c009825cc63693046cc6a1de0aa463c70ac
Matheus <mdonizetecardoso@gmail.com>
2024-10-21 12:41:50 -0300

    Incluindo a edição de usuário

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -5,5 +5,6 @@ const userController = require('../controllers/userController')
 router.post('/user', userController.createUser)
 router.get('/user', userController.getUsers)
 router.delete('/user/:id',userController.deleteUser)
+router.put('/user/:id',userController.updateUser)
 
 module.exports = router
